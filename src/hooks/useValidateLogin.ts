90e9192506f43b7baa61d66a1a996b5e2329872e
Akhilesh Sankhyan <iamakhilesh1402@gmail.com>
2024-05-16 15:46:30 +0530

    adde translation for errors

--- src/hooks/useValidateLogin.ts
+++ src/hooks/useValidateLogin.ts
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react';
+import {useTranslation} from 'react-i18next';
 
 const useValidateLogin = (
   enteredEmail: string,
@@ -6,12 +7,13 @@ const useValidateLogin = (
 ): [string | null, string | null] => {
   const [emailError, setEmailError] = useState<string | null>(null);
   const [passwordError, setPasswordError] = useState<string | null>(null);
+  const {t} = useTranslation();
 
   function validateEmail() {
     const emailRegex =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (enteredEmail && !emailRegex.test(enteredEmail)) {
-      setEmailError('Please enter a valid email address.');
+      setEmailError(t('emailError'));
       return;
     }
     setEmailError(null);
@@ -19,7 +21,7 @@ const useValidateLogin = (
 
   function validatePassword() {
     if (enteredPassword && enteredPassword.length < 8) {
-      setPasswordError('Password must be 8 characters long.');
+      setPasswordError(t('pwdError'));
       return;
     }
     setPasswordError(null);
